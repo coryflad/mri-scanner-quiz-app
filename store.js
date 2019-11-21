c8344a186e05cdf92e6bb2ea89fb0a00d08fc07e
Cory Fladhammer <cjfladham@hotmail.com>
2019-11-20 19:53:08 -0600

    first question displayed

--- store.js
+++ store.js
@@ -1,71 +1,61 @@
-let STORE = {
+const STORE = {
     questions: [//1
         {
             question: 'The biggest and most important component of an MRI system is the:',
-            answers: [
+            options: [
                 'Bore',
                 'Imager',
                 'Magnet',
-                'Table',
+                'Table'
             ],
-            correctAnswer: 'Magnet'
+            answer: 'Magnet'
         },
         //2
         {
             question: 'The first MRI examination was performed on a human being in:',
-            answers: [
+            options: [
                 '1967',
                 '1985',
                 '1977',
-                '1984',
+                '1984'
             ],
-            correctAnswer: '1977',
+            answer: '1977'
         },
         //3
         {
             question: 'For the purpose of an MRI, which element within the body is most important?',
-            answers: [
+            options: [
                 'Oxygen',
                 'Carbon',
                 'Hydrogen',
-                'Zinc',
+                'Zinc'
             ],
-            correctAnswer: 'Hydrogen',   
+            answer: 'Hydrogen'
         },
         //4
         {
             question: 'An MRI system uses an RF pulse to change the:',
-            answers: [
+            options: [
                 'spin of atoms\' nuclei',
                 'shape of the nuclei',
                 'amount of metal in a person\'s cells',
-                'None of these choices is correct',
+                'None of these choices is correct'
             ],
-            correctAnswer: 'spin of atoms\' nuclei',  
+            answer: 'spin of atoms\' nuclei'
         },
         //5
         {
             question: 'What does an MRI system employ to convert mathematical data into a picture?',
-            answers: [
+            options: [
                 'RF pulse converter',
                 'Fourier transform',
                 'electron precession',
-                'flux capacitor',
+                'flux capacitor'
             ],
-            correctAnswer: 'Fourier transform',  
-        },
-
+            answer: 'Fourier transform'
+        }
     ],
+    currentQuestion: 0,
+    score: 0
 };
-
-let myDog = {
-    name: 'Stanley',
-    color: 'gray',
-
-    stanleyIs: function() {
-        return `${this.name} is a Minature Schnauzer and his fur is ${this.color}`;
-    }
-}
-
-console.log(myDog.stanleyIs());
 
