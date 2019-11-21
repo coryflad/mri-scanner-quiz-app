2d7dc33e42ad63238dcdbed5d8e954065e0219b4
Cory Fladhammer <cjfladham@hotmail.com>
2019-11-20 20:30:41 -0600

    updated store structure

--- store.js
+++ store.js
@@ -1,61 +1,64 @@
-const STORE = {
-    questions: [//1
-        {
-            question: 'The biggest and most important component of an MRI system is the:',
-            options: [
-                'Bore',
-                'Imager',
-                'Magnet',
-                'Table'
-            ],
-            answer: 'Magnet'
-        },
-        //2
-        {
-            question: 'The first MRI examination was performed on a human being in:',
-            options: [
-                '1967',
-                '1985',
-                '1977',
-                '1984'
-            ],
-            answer: '1977'
-        },
-        //3
-        {
-            question: 'For the purpose of an MRI, which element within the body is most important?',
-            options: [
-                'Oxygen',
-                'Carbon',
-                'Hydrogen',
-                'Zinc'
-            ],
-            answer: 'Hydrogen'
-        },
-        //4
-        {
-            question: 'An MRI system uses an RF pulse to change the:',
-            options: [
-                'spin of atoms\' nuclei',
-                'shape of the nuclei',
-                'amount of metal in a person\'s cells',
-                'None of these choices is correct'
-            ],
-            answer: 'spin of atoms\' nuclei'
-        },
-        //5
-        {
-            question: 'What does an MRI system employ to convert mathematical data into a picture?',
-            options: [
-                'RF pulse converter',
-                'Fourier transform',
-                'electron precession',
-                'flux capacitor'
-            ],
-            answer: 'Fourier transform'
-        }
-    ],
-    currentQuestion: 0,
-    score: 0
-};
+const STORE = [
+    //1
+    {
+        question: 'The biggest and most important component of an MRI system is the:',
+        options: [
+            'Bore',
+            'Imager',
+            'Magnet',
+            'Table'
+        ],
+        answer: 'Magnet',
+        correctAnswerExplaination: 'As its name suggests, a magnetic resonance imaging (MRI) system has a magnet at is core. This magnet is so powerful it can suck metal objects into the tube (and out of a person\'s body).'
+    },
+    //2
+    {
+        question: 'The first MRI examination was performed on a human being in:',
+        options: [
+            '1967',
+            '1985',
+            '1977',
+            '1984'
+        ],
+        answer: '1977',
+        correctAnswerExplaination: ''
+    },
+    //3
+    {
+        question: 'For the purpose of an MRI, which element within the body is most important?',
+        options: [
+            'Oxygen',
+            'Carbon',
+            'Hydrogen',
+            'Zinc'
+        ],
+        answer: 'Hydrogen',
+        correctAnswerExplaination: ''
+    },
+    //4
+    {
+        question: 'An MRI system uses an RF pulse to change the:',
+        options: [
+            'spin of atoms\' nuclei',
+            'shape of the nuclei',
+            'amount of metal in a person\'s cells',
+            'None of these choices is correct'
+        ],
+        answer: 'spin of atoms\' nuclei',
+        correctAnswerExplaination: ''
+    },
+    //5
+    {
+        question: 'What does an MRI system employ to convert mathematical data into a picture?',
+        options: [
+            'RF pulse converter',
+            'Fourier transform',
+            'electron precession',
+            'flux capacitor'
+        ],
+        answer: 'Fourier transform',
+        correctAnswerExplaination: ''
+    }
+];
+
 
