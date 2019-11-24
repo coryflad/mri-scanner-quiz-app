1c73553d363df152b2898abbf59b8965d30f5b50
Cory Fladhammer <cjfladham@hotmail.com>
2019-11-23 19:16:19 -0600

    added questions updated styling

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //question database
 const STORE = [
-  {
+  {//1
     question: 'The biggest and most important component of an MRI system is the:',
     answers: [
       'Bore',
       'Imager',
       'Magnet',
-      'Tecnologist'
+      'Technologist'
     ],
     correctAnswer: 'Magnet',
     answerExplaination: 'As its name suggests, a magnetic resonance imaging (MRI) system has a magnet at is core. This magnet is so powerful it can suck metal objects into the tube (and out of a person\'s body).'
   },
-  {
+  {//2
     question: 'The first MRI examination was performed on a human being in:',
     answers: [
       '1967',
@@ -22,7 +22,7 @@ const STORE = [
     correctAnswer: '1977',
     answerExplaination: 'The 1977 conduction of the first-ever MRI exam was a breakthrough in medical science. It allowed doctors to examine the interior of the human body in a way that had been impossible.'
   },
-  {
+  {//3
     question: 'For the purpose of an MRI, which element within the body is most important?',
     answers: [
       'Oxygen',
@@ -33,7 +33,7 @@ const STORE = [
     correctAnswer: 'Hydrogen',
     answerExplaination: 'Hydrogen atoms have one proton and a large magnetic moment. This means they react strongly to magnetic fields and are ideal for examination in an MRI machine.',
   },
-  {
+  {//4
     question: 'An MRI system uses an RF pulse to change the:',
     answers: [
       'spin of atoms\' nuclei',
@@ -43,7 +43,7 @@ const STORE = [
     correctAnswer: 'spin of atoms\' nuclei',
     answerExplaination: 'An MRI\'s RF pulse changes the spin, or precession, of atoms\' nuclei. The hydrogen atoms line up with the direction of the MRI\'s magnetic field.'
   },
-  {
+  {//5
     question:
       'What does an MRI system employ to convert mathematical data into a picture?',
     answers: [
@@ -55,6 +55,68 @@ const STORE = [
     correctAnswer:
       'Fourier transform',
     answerExplaination: 'Fourier transform uses mathematical functions to change the data retrieved by the MRI system into a picture that a doctor can examine to make a diagnosis.'
+  },
+  {//6
+    question:
+      'The most common type of magne used in a MRI system is the:',
+    answers: [
+      'resistive magnet',
+      'permanent magnet',
+      'superconducting magnet',
+      'the magnet at the North Pole'
+    ],
+    correctAnswer:
+      'superconducting magnet',
+    answerExplaination: 'Although all three types of magnets can be used in an MRI machine, the superconducting magnet is the most common. In general, large enough resistive magnets are too expensive, and powerful enough permanent magnets are too big.'
+  },
+  {//7
+    question:
+      'In a superconducting magnet, the coils are bathed in liquid helium:',
+    answers: [
+      'to keep the system from overheating',
+      'to keep the area around the coils sterile',
+      'to reduce the amount of resistance in the wire',
+      'to help keep the scan room cool'
+    ],
+    correctAnswer:
+      'to reduce the amount of resistance in the wire',
+    answerExplaination: 'At normal temperatures, it would take lots of power to send electricity through all the wire in a superconducting magnet. But at the very cold temperature of liquid helium, there\'s almost no resistance in the wire, greatly reducing the need for power.'
+  },
+  {//8
+    question:
+      'MRI scans tend to be very noisy. The noise comes from:',
+    answers: [
+      'opposition of the main magnetic field to the current in the gradient magnets\' wires',
+      'stray particles within the system being thrown about by the magnetic field',
+      'movement of the magnet from place to place'
+    ],
+    correctAnswer:
+      'opposition of the main magnetic field to the current in the gradient magnets\' wires',
+    answerExplaination: 'The stronger the main magnetic field in an MRI, the louder the banging will be when it encounters the current in the gradient magnets\' wires. For this reason, many MRI centers give patients earphones or earplugs.'
+  },
+  {//9
+    question:
+      'What component of an MRI system allows it to choose exactly where in the body to acquire an image?',
+    answers: [
+      'Gradient magnets',
+      'Bore',
+      'Contrast injector'
+    ],
+    correctAnswer:
+      'Gradient magnets',
+    answerExplaination: ' Gradient magnets are small magnets that change the field within an MRI system. When turned on and off very rapidly, they essentially change the focus of the overall field.'
+  },
+    {//10
+      question:
+      'What unit of measure are MRI scanners calibrated to?',
+    answers: [
+      'Centimeters',
+      'Kelvin',
+      'Tesla'
+    ],
+    correctAnswer:
+      'Tesla',
+    answerExplaination: 'MRI machines are calibrated in Tesla units. Nikola Tesla is associated with the discovery of the rotating magnetic field.'
   },
 ];
 
@@ -69,7 +131,7 @@ function generateQuestion() {
   } else {
     $('.questionBox').hide();
     finalScore();
-    $('.questionNumber').text(5);
+    $('.questionNumber').text(10);
   }
 }
 
@@ -152,7 +214,7 @@ function createThing(questionIndex) {
 function correctAnswer() {
   $('.response').html(
     `<h3>Correct!</h3>
-      <p class="sizeMe">You could maybe be as smart as Nikola Tesla!</p>
+      <p class="sizeMe">You might be as smart as Nikola Tesla!</p>
       <button type="button" class="nextButton button">Next</button>`
   );
   updateScore();
@@ -161,7 +223,7 @@ function correctAnswer() {
 //resulting feedback if a selected answer is incorrect
 function wrongAnswer() {
   $('.response').html(
-    `<h3>Incorrect answer...</h3>
+    `<h3>Incorrect answer!</h3>
     <p class="sizeMe">The correct answer is:</p>
     <p class="sizeMe">${STORE[questionNumber].correctAnswer}</p>
     <p class="sizeMe">${STORE[questionNumber].answerExplaination}</p>
@@ -208,7 +270,7 @@ function finalScore() {
   }
   return $('.final').html(
     `<h3>${array[0]}</h3>
-        <h3>Your score is ${score} /5</h3>
+        <h3>Your score is ${score} /10</h3>
         <p class="sizeMe">${array[1]}</p>
         <button type="submit" class="restartButton button">Restart</button>`
   );
